0de12f119dc3a0e9ab810465c77106ac3360c647
richard579 <richardbeng358@outlook.com>
2023-09-23 07:34:56 -0700

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
                 "task": t.get("title"),
                 "completed": t.get("completed"),
                 "username": username
-        ) for t in todos]
+            } for t in todos]}, jsonfile)
