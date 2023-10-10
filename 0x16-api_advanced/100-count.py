8b8c3f5318471ab784fe1bb0cbbca6a848738741
richard579 <richardbeng358@outlook.com>
2023-10-10 05:54:10 -0700

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Module for a function that queries the reddit API recursively."""
+""" Module for a function that queries the Reddit API recursively."""
 
 
 import requests
 
 
 def count_words(subreddit, word_list, after='', word_dict={}):
-    """A function that queries the Reddit API parses the title of
+    """ A function that queries the Reddit API parses the title of
     all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces. Javascript should
-    count as javascript, but java should not). If no posts match
-    or the subreddit is invalid, it prints nothing."""
+    (case-insensitive, delimited by spaces.
+    Javascript should count as javascript, but java should not).
+    If no posts match or the subreddit is invalid, it prints nothing.
+    """
 
     if not word_dict:
         for word in word_list:
